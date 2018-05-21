301a4f5456236a4d021cca4d41380cd605f2d3f7
Jonathan Abantao <jonabantao@gmail.com>
2018-05-21 16:38:11 -0700

    Complete all user service method tests and pass them

--- frontend/src/app/services/user.service.spec.ts
+++ frontend/src/app/services/user.service.spec.ts
@@ -11,6 +11,8 @@ describe('UserService', () => {
   let dummyUser: User;
   let dummyUsers: User[];
 
+  const USER_BASE_URL = '/api/users/';
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
@@ -31,18 +33,70 @@ describe('UserService', () => {
     dummyUsers = [dummyUser];
   }));
 
-  describe('getUsers', () => {
-    it('should return successfully', () => {
+  describe('getUsers()', () => {
+    it('should have to an endpoint for method and return array of users', () => {
       service.getUsers()
         .subscribe(res => {
           expect(res).toEqual(dummyUsers);
           expect(res.length).toEqual(1);
         });
 
-      const mock = httpMock.expectOne('/api/users/');
+      const mock = httpMock.expectOne(USER_BASE_URL);
       expect(mock.request.method).toBe('GET');
 
       mock.flush(dummyUsers);
+      httpMock.verify();
+    });
+  });
+
+  describe('getUser()', () => {
+    it('should have to an endpoint for method and return user', () => {
+      service.getUser(dummyUserId)
+        .subscribe(res => {
+          expect(res).toEqual(dummyUser);
+        });
+
+      const mock = httpMock.expectOne(`${USER_BASE_URL}${dummyUserId}`);
+      expect(mock.request.method).toBe('GET');
+
+      mock.flush(dummyUser);
+      httpMock.verify();
+    });
+  });
+
+  describe('deleteUser()', () => {
+    it('should have to an endpoint for method', () => {
+      service.deleteUser(dummyUserId)
+        .subscribe(res => {
+          expect(res).toEqual('OK');
+        });
+
+      const mock = httpMock.expectOne(`${USER_BASE_URL}${dummyUserId}`);
+      expect(mock.request.method).toBe('DELETE');
+
+      mock.flush('OK');
+      httpMock.verify();
+    });
+  });
+
+  describe('updateUser()', () => {
+    it('should have to an endpoint for method and return updated user', () => {
+      const updatedUser = {
+        id: 1,
+        username: 'tester',
+        firstName: 'firstupdate',
+        lastName: 'lastname',
+      };
+
+      service.updateUser(updatedUser)
+        .subscribe(res => {
+          expect(res).toEqual(updatedUser);
+        });
+
+      const mock = httpMock.expectOne(`${USER_BASE_URL}${updatedUser.id}`);
+      expect(mock.request.method).toBe('PATCH');
+
+      mock.flush(updatedUser);
       httpMock.verify();
     });
   });
