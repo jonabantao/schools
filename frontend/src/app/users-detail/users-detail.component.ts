71e03c63ab1d9ae875b7dd2fe6a01ecea4a206b5
Jonathan Abantao <jonabantao@gmail.com>
2018-05-21 11:44:27 -0700

    Setup front end GET for individual user

--- frontend/src/app/users-detail/users-detail.component.ts
+++ frontend/src/app/users-detail/users-detail.component.ts
@@ -1,4 +1,7 @@
+import { User } from './../models/user.model';
+import { UserService } from './../services/user.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-users-detail',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users-detail.component.css']
 })
 export class UsersDetailComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(
+    private usersService: UserService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit() {
-  }
+    const userId = Number.parseInt(this.route.snapshot.paramMap.get('userId'), 10);
 
+    this.usersService.getUser(userId)
+      .subscribe(user => this.user = user);
+  }
 }
