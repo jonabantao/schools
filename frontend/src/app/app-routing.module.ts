9d5eaeaf8db15b3c8fddc49da359b086215780c6
Jonathan Abantao <jonabantao@gmail.com>
2018-05-24 15:54:21 -0700

    Rename app and add validator errors to signup form

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -9,17 +9,13 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AuthGuard } from './guards/auth.guard';
 import { ManageUsersComponent } from './manage-users/manage-users.component';
 import { UsersDetailComponent } from './users-detail/users-detail.component';
-import { LoggedGuard } from './guards/logged.guard';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
-  { path: 'login', component: LoginComponent, canActivate: [LoggedGuard] },
-  { path: 'signup', component: SignupComponent, canActivate: [LoggedGuard] },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'manage-users', component: ManageUsersComponent, canActivate: [AuthGuard] },
   { path: 'users/:userId', component: UsersDetailComponent }
 ];
-
 
 @NgModule({
   exports: [RouterModule],
--- frontend/src/app/auth/signup/signup.component.ts
+++ frontend/src/app/auth/signup/signup.component.ts
@@ -12,6 +12,11 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 })
 export class SignupComponent implements OnInit {
   sessionForm: FormGroup;
+  usernameControl: FormControl;
+  firstNameControl: FormControl;
+  lastNameControl: FormControl;
+  emailControl: FormControl;
+  passwordControl: FormControl;
   hide: boolean;
 
   constructor(
@@ -22,16 +27,34 @@ export class SignupComponent implements OnInit {
 
   ngOnInit() {
     this.hide = true;
+    this.usernameControl = this.formBuilder.control('', [Validators.required]);
+    this.firstNameControl = this.formBuilder.control('', Validators.required);
+    this.lastNameControl = this.formBuilder.control('', Validators.required);
+    this.emailControl = this.formBuilder.control('', [Validators.required, Validators.email]);
+    this.passwordControl = this.formBuilder.control('', [Validators.required, Validators.minLength(6)]);
 
     this.sessionForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      password: [
-        '',
-        [Validators.required, Validators.minLength(6)]
-      ],
+      username: this.usernameControl,
+      firstName: this.firstNameControl,
+      lastName: this.lastNameControl,
+      email: this.emailControl,
+      password: this.passwordControl,
     });
+  }
+
+  getFieldErrorMessage(field) {
+    return this[field].hasError('required') ? 'Field cannot be blank' : '';
+  }
+
+  getEmailErrorMessage() {
+    return this.emailControl.hasError('required') ? 'You must enter an email' :
+      this.emailControl.hasError('email') ? 'You must enter a valid email' : '';
+  }
+
+  getPasswordErrorMessage() {
+    return this.passwordControl.hasError('required') ? 'Password cannot be blank' :
+      this.passwordControl.hasError('minlength') ? 'Password must be greater than 6 characters' :
+        '';
   }
 
   signup(form: FormGroup) {
