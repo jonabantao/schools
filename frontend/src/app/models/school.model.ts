81650f5de131ae8b394f7ee2eb9d3e749d875f18
Jonathan Abantao <jonabantao@gmail.com>
2018-06-08 23:43:21 -0700

    Switch school interface to semicolon delimiters

--- frontend/src/app/models/school.model.ts
+++ frontend/src/app/models/school.model.ts
@@ -1,9 +1,9 @@
 export interface School {
-    campusName: string,
-    campusAddress: string,
-    gradeRange: string,
+    campusName: string;
+    campusAddress: string;
+    gradeRange: string;
     location_1: {
         type: string,
         coordinations: [number, number],
-    }
+    };
 };
